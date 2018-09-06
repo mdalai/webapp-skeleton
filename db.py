9048fd6456f445642b0f2ca82c05fd06fc2cbc61
Dalai <ndalai@cuw.edu>
2018-09-06 15:30:02 -0500

    create db

--- db.py
+++ db.py
@@ -3,8 +3,13 @@ import sqlite3
 def create_db(db_path):
   conn = sqlite3.connect(db_path)
   cur = conn.cursor()
+  # drop tables before create
+  cur.execute("DROP TABLE IF EXISTS applications")
+  cur.execute("DROP TABLE IF EXISTS users")
+  cur.execute("DROP TABLE IF EXISTS userapps")
+
   cur.execute('''CREATE TABLE IF NOT EXISTS applications
-                  (id INTEGER PRIMARY KEY,
+                  (pid INTEGER PRIMARY KEY,
                    name TEXT,
                    description TEXT,
                    color TEXT,
@@ -12,32 +17,32 @@ def create_db(db_path):
                    link TEXT )  ''')
   
   cur.execute('''CREATE TABLE IF NOT EXISTS users
-                  (id INTEGER PRIMARY KEY,
+                  (pid INTEGER PRIMARY KEY,
                    login TEXT,
                    password TEXT )  ''')
 
   cur.execute('''CREATE TABLE IF NOT EXISTS userapps
-                  (id INTEGER PRIMARY KEY,
-                   order INTEGER,
+                  (pid INTEGER PRIMARY KEY,
+                   placeorder INTEGER,
                    userid INTEGER,
                    appid INTEGER,
-                   FOREIGN KEY(userid) REFERENCES users(id),
-                   FOREIGN KEY(appid) REFERENCES applications(id)
+                   FOREIGN KEY (userid) REFERENCES users (pid),
+                   FOREIGN KEY (appid) REFERENCES applications (pid)
                     )  ''')
 
   apps = [
-    (NULL,'Google','Search Engine','Red',1,'http://www.google.com'),
-    (NULL,'Wisc','UW homepage','Blue',0,'http://www.wisc.edu'),
-    (NULL,'GLBRC','Great Lakes Bioenergy Research Center','Yellow',1,'http://www.glbrc.org'),
-    (NULL,'WEI','Wisconsin Energy Institute','Green',0,'https://energy.wisc.edu/'),
-    (NULL,'Twitter','Twitter','Purple',0,'https://twitter.com/'), 
+    (None,'Google','Search Engine','Red',1,'http://www.google.com'),
+    (None,'Wisc','UW homepage','Blue',0,'http://www.wisc.edu'),
+    (None,'GLBRC','Great Lakes Bioenergy Research Center','Yellow',1,'http://www.glbrc.org'),
+    (None,'WEI','Wisconsin Energy Institute','Green',0,'https://energy.wisc.edu/'),
+    (None,'Twitter','Twitter','Purple',0,'https://twitter.com/'), 
   ]
   cur.executemany('INSERT INTO applications VALUES (?,?,?,?,?,?)', apps)
 
   users = [
-    (NULL, 'user1', 'glbrcpass'),
-    (NULL, 'user2', 'glbrcpass'),
-    (NULL, 'user3', 'glbrcpass'),
+    (None, 'user1', 'glbrcpass'),
+    (None, 'user2', 'glbrcpass'),
+    (None, 'user3', 'glbrcpass'),
   ]
   cur.executemany('INSERT INTO users VALUES (?,?,?)', users)
   
@@ -50,10 +55,33 @@ def desc_db(db_path):
   conn = sqlite3.connect(db_path)
   c = conn.cursor()
 
+  # databse descriptions
+  tables = c.execute("SELECT name FROM sqlite_master WHERE type='table'").fetchall()
+  print('-'*5 + ' TABLES '+ '-'*30)
+  for t in tables:
+    print(t)
+
+  print('-'*5 + ' TABLE INFO '+ '-'*30)
+  meta = c.execute("PRAGMA table_info('applications')")
+  for r in meta:
+    print(r)
+  print('-'*10)
+  meta = c.execute("PRAGMA table_info('users')")
+  for r in meta:
+    print(r)
+  print('-'*10)
+  meta = c.execute("PRAGMA table_info('userapps')")
+  for r in meta:
+    print(r)
+  
+  #print(c.fetchall())
+
+  print('-'*5 + ' TABLE DATA '+ '-'*30)
+
   for row in c.execute('SELECT * FROM applications'):
         print(row)
 
-  print('-'*30)
+  print('-'*10)
 
   for row in c.execute('SELECT * FROM users'):
         print(row)

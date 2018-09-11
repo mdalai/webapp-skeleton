fb083cbf608f31428a3a8fe8829c4348f4f4241c
Dalai <ndalai@cuw.edu>
2018-09-11 11:28:05 -0500

    update gitignore

--- home/models.py
+++ home/models.py
@@ -24,8 +24,6 @@ class Applications(models.Model):
     def __str__(self):
         return self.name
 
-    def count(self):
-        return 10
 '''
 class Users(models.Model):
     pid = models.IntegerField(primary_key=True)

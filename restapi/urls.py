b6627da5c49329b0e117dd170921e19e9e715fb4
Mingat Dalai <mingat.dalai@gmail.com>
2026-09-18 16:16:01 -0500

    Upgrade dependencies to patch security vulnerabilities and update compatibility
    - Upgrade Django (>=5.2.16), djangorestframework (>=3.17.2), and gunicorn (>=22.0.0) in requirements.txt
    - Replace deprecated django.conf.urls.url with django.urls.re_path
    - Replace removed HttpRequest.is_ajax() with headers check in AjaxTemplateMixin
    - Configure DEFAULT_AUTO_FIELD in settings.py

--- restapi/urls.py
+++ restapi/urls.py
@@ -1,18 +1,17 @@
-from django.urls import path, include
+from django.urls import path, re_path, include
 from .views import UserViewSet #,ListApplicationsView
 from  restapi import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from rest_framework import routers
-from django.conf.urls import url
 
 
 urlpatterns = [
     #path('apps/', ListApplicationsView.as_view(), name="apps-all"),
     #path('apps_def/', views.app_list, name="apps-list-def"),
-    #url(r'^apps_def/(?P<pk>[0-9]+)/$', views.app_detail, name="app-detail"),
+    #re_path(r'^apps_def/(?P<pk>[0-9]+)/$', views.app_detail, name="app-detail"),
     path('apps/', views.AppList.as_view(), name="apps-list"),
-    url(r'^apps/(?P<pk>[0-9]+)/$', views.AppDetail.as_view(), name="app-detail"),
+    re_path(r'^apps/(?P<pk>[0-9]+)/$', views.AppDetail.as_view(), name="app-detail"),
     #path('users/', UserViewSet.as_view(), name="users-all"),
 ]
 
@@ -26,7 +25,7 @@ urlpatterns = format_suffix_patterns(urlpatterns)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns += [
-    #url(r'^', include(router.urls)),
-    url(r'^users/$', views.UserList.as_view(), name='customuser-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='customuser-detail'),
+    #re_path(r'^', include(router.urls)),
+    re_path(r'^users/$', views.UserList.as_view(), name='customuser-list'),
+    re_path(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='customuser-detail'),
 ]
